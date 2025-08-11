faa7bacc3eae715cc4e4b5b1cb979981819e8409
Mohe <86363939+MoheEdeen@users.noreply.github.com>
2025-08-11 12:19:02 -0400

    push

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode, Dispatch, SetStateAction } from "react";
-
 import {
   useDragControls,
   useMotionValue,
@@ -7,93 +6,70 @@ import {
   motion,
 } from "framer-motion";
 import useMeasure from "react-use-measure";
+import { createPortal } from "react-dom";
 
 interface Props {
   open: boolean;
-
   setOpen: Dispatch<SetStateAction<boolean>>;
-
   children?: ReactNode;
 }
 
 export const DragCloseDrawer = ({ open, setOpen, children }: Props) => {
   const [scope, animate] = useAnimate();
-
   const [drawerRef, { height }] = useMeasure();
-
   const y = useMotionValue(0);
-
   const controls = useDragControls();
 
   const handleClose = async () => {
-    animate(scope.current, {
-      opacity: [1, 0],
-    });
-
+    animate(scope.current, { opacity: [1, 0] });
     const yStart = typeof y.get() === "number" ? y.get() : 0;
-
-    await animate("#drawer", {
-      y: [yStart, height],
-    });
-
+    await animate("#drawer", { y: [yStart, height] });
     setOpen(false);
   };
 
-  return (
-    <>
-      {open && (
+  if (typeof document === "undefined") return null;
+
+  return createPortal(
+    open && (
+      <motion.div
+        ref={scope}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        onClick={handleClose}
+        className="fixed inset-0 z-50"
+      >
         <motion.div
-          ref={scope}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          onClick={handleClose}
-          className="fixed inset-0 z-50"
+          id="drawer"
+          ref={drawerRef}
+          onClick={(e) => e.stopPropagation()}
+          initial={{ y: "100%" }}
+          animate={{ y: "0%" }}
+          transition={{ ease: "easeInOut" }}
+          className="absolute bottom-0 h-[75vh] w-full overflow-hidden rounded-t-3xl bg-neutral-900"
+          style={{ y }}
+          drag="y"
+          dragControls={controls}
+          onDragEnd={() => {
+            if (y.get() >= 100) handleClose();
+          }}
+          dragListener={false}
+          dragConstraints={{ top: 0, bottom: 0 }}
+          dragElastic={{ top: 0, bottom: 0.5 }}
         >
-          <motion.div
-            id="drawer"
-            ref={drawerRef}
-            onClick={(e) => e.stopPropagation()}
-            initial={{ y: "100%" }}
-            animate={{ y: "0%" }}
-            transition={{
-              ease: "easeInOut",
-            }}
-            className="absolute bottom-0 h-[75vh] w-full overflow-hidden rounded-t-3xl bg-neutral-900"
-            style={{ y }}
-            drag="y"
-            dragControls={controls}
-            onDragEnd={() => {
-              if (y.get() >= 100) {
-                handleClose();
-              }
-            }}
-            dragListener={false}
-            dragConstraints={{
-              top: 0,
-
-              bottom: 0,
-            }}
-            dragElastic={{
-              top: 0,
-
-              bottom: 0.5,
-            }}
-          >
-            <div className="absolute left-0 right-0 top-0 z-10 flex justify-center bg-neutral-900 p-4">
-              <button
-                onPointerDown={(e) => {
-                  controls.start(e);
-                }}
-                className="h-2 w-14 cursor-grab touch-none rounded-full bg-neutral-700 active:cursor-grabbing"
-              ></button>
-            </div>
-
-            <div className="relative z-0 h-full overflow-y-scroll p-4 pt-12">
-              {children}
-            </div>
-          </motion.div>
+          <div className="absolute left-0 right-0 top-0 z-10 flex justify-center bg-neutral-900 p-4">
+            <button
+              onPointerDown={(e) => {
+                controls.start(e);
+              }}
+              className="h-2 w-14 cursor-grab touch-none rounded-full bg-neutral-700 active:cursor-grabbing"
+            ></button>
+          </div>
+          <div className="relative z-0 h-full overflow-y-scroll p-4 pt-12">
+            {children}
+          </div>
         </motion.div>
-      )}
-    </>
+      </motion.div>
+    ),
+    document.body
   );
 };
